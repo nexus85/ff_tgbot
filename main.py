3fb9c26621702ca78852a5792cc92c9076babc63
nexus85 <mx38@mail.ru>
2021-08-04 00:58:24 +0300

    add env

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import time
-
+import os
 from telegram import Bot
 from loguru import logger
 from decouple import config
 
-BOT_TOKEN = config('BOT_TOKEN', cast=str)
-CHAT_ID = config('CHAT_ID', cast=str)
-
-
+# BOT_TOKEN = config('BOT_TOKEN', cast=str)
+# CHAT_ID = config('CHAT_ID', cast=str)
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+CHAT_ID = os.getenv('CHAT_ID')
 def main():
     bot = Bot(token=BOT_TOKEN)
 
